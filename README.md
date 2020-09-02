2825d523f851fee81ec3f5bea1e7069779c033fb
John <john.mandia@gmail.com>
2020-09-02 12:53:44 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# openfin-multi-instance-window
-Created with CodeSandbox
+# Multi Window Instance Demo
+
+This is an example of having the same url launched every time the user launches the app (a bit like opening a new browser tab).
---
+++ provider-window-init.js
@@ -0,0 +1,34 @@
+const initOpenFinParamListener = () => {
+  const openMainWindow = () => {
+    let winOption = {
+      name: "child-" + Date.now(),
+      defaultWidth: 300,
+      defaultHeight: 300,
+      url:
+        "https://cdn.openfin.co/docs/javascript/stable/tutorial-Window.create.html",
+      frame: true,
+      autoShow: true
+    };
+    fin.Window.create(winOption)
+      .then((win) => {
+        // if you need to do anything with the window
+      })
+      .catch((reason) => {
+        console.log("There was an error launching the window: " + reason);
+      });
+  };
+
+  fin.desktop.main((userAppConfigArgs) => {
+    openMainWindow();
+  });
+
+  let app = fin.Application.getCurrentSync();
+  // If app is already running parameters are passed through the “run-requested” event
+  app.addListener("run-requested", function (event) {
+    openMainWindow();
+  });
+};
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  initOpenFinParamListener();
+});
